fe57b6eabc5a3cfd5422cf569dbd238ac54a7105
Aleksei Loskutov <git@zergular.com>
2021-03-21 01:32:19 +0300

    Fix errors

--- internal/repository/postgres/file.go
+++ internal/repository/postgres/file.go
@@ -27,7 +27,7 @@ func (r *Repo) FindFiles(ids []uint) ([]*model.File, error) {
 	var files []*model.File
 	if err := r.DB.
 		Where("id in (?)", ids).
-		Find(files).Error; err != nil {
+		Find(&files).Error; err != nil {
 		return nil, errs.NewStack(err)
 	}
 	return files, nil
